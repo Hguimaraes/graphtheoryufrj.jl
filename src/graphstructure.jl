775df41a947e07925958013ad0327d49a47cb7c0
Hguimaraes <hguimaraes@poli.ufrj.br>
2015-11-21 01:31:06 -0200

    Major upgrade: Implement the simple graph and adding a graph example.

--- src/graphstructure.jl
+++ src/graphstructure.jl
@@ -14,24 +14,37 @@ type simpleGraph
 	num_vertex::Int64
 	num_edge::Int64
 	mean_degree::Float64
-	empirical_dist::Array{Float64,1}
+	empirical_dist::Array{Float64, 1}
+	Graph::Array{Int64, 2}
 
 	function simpleGraph(filename::ASCIIString)
 	"""
 	"""	
-		println("Inside the Class Graph")
+		# Instantiate the Class
+		this = new()
+
+		# Read the text file and get direct information.
+		graphtxt = readdlm(filename)
+		this.num_vertex = graphtxt[1]
+		this.Graph = graphtxt[2:end, :]
+
+		# Calculate the others information
+		this.num_edge = size(this.Graph)[1]
+		#this.mean_degree = 
+		#this.empirical_dist =
+
+		return this
 	end
 
 	function make_report()
 	"""
 	"""
-
 	end
 end
 
 #
 type demoGraph
-	function Graph()
+	function demoGraph()
 		println("Inside the Class Demo Graph")
 	end
 end
