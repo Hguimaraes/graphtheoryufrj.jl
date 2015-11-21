13f06524f3ecbdc39fd4d6d4991600b29713f7bd
Hguimaraes <hguimaraes@poli.ufrj.br>
2015-11-21 02:16:41 -0200

    Adding the method to transform the graph into an adj matrix.

--- src/graphstructure.jl
+++ src/graphstructure.jl
@@ -16,6 +16,7 @@ type simpleGraph
 	mean_degree::Float64
 	empirical_dist::Array{Float64, 1}
 	Graph::Array{Int64, 2}
+	make_report::Function
 
 	function simpleGraph(filename::ASCIIString)
 	"""
@@ -30,15 +31,22 @@ type simpleGraph
 
 		# Calculate the others information
 		this.num_edge = size(this.Graph)[1]
-		#this.mean_degree = 
-		#this.empirical_dist =
+		##this.mean_degree = ??
+		##this.empirical_dist = ??
 
+		this.make_report = function(outfilename::ASCIIString)
+		"""
+		"""
+			# Open the file and write the information on it.
+			outfile = open(outfilename, "w")
+			write(outfile, string("# n = ", this.num_vertex,"\n"))
+			write(outfile, string("# m = ", this.num_edge,"\n"))
+			write(outfile, string("# d_medio = ", this.mean_degree,"\n"))
+
+			# Close and save the file.
+			close(outfile)
+		end
 		return this
-	end
-
-	function make_report()
-	"""
-	"""
 	end
 end
 
@@ -51,10 +59,25 @@ end
 
 function asAdjList(G::simpleGraph)
 """
+@description:
+@param G::simpleGraph :
+@output:
 """
 end
 
 function asAdjMatrix(G::simpleGraph)
 """
-"""	
+@description:
+@param G::simpleGraph :
+@output:
+"""
+	vertex = simpleGraph.num_vertex
+	edges = simpleGraph.num_edge
+	adjmatrix = zeros(Int64, vertex, vertex)
+
+	@inbounds @simd for i in 1:edges
+    	rnum, cnum = simpleGraph.Graph[i,:]
+    	adjmatrix[rnum,cnum] = 1
+    end
+	return adjmatrix
 end
