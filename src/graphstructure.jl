8d094a6647bfc013f8e47b474b3b26a2c0dd144f
Hguimaraes <hguimaraes@poli.ufrj.br>
2015-11-27 19:06:22 -0200

    Fixing the adjMatrix method and improving documentation.

--- src/graphstructure.jl
+++ src/graphstructure.jl
@@ -4,11 +4,13 @@
 #
 #
 
-using DataStructures
-
-#
 type simpleGraph
 """
+@brief: This composite type is an abstraction of a simple undirected
+	graph. It is constructed through a text file contain details about it's
+	edges and vertices. To a specific use in search algorithms as BFS and DFS
+	you can use a method to transform this abstraction in a more concrete
+	data structure as Adjacency List or Adjacency Matrix.
 """
 	# Properties of the Graph
 	num_vertex::Int64
@@ -20,6 +22,12 @@ type simpleGraph
 
 	function simpleGraph(filename::ASCIIString)
 	"""
+	@brief: Constructor of the composite type simpleGraph. This method
+		read the text file and construct the Graph as a List with integer
+		numbers. Also extract basic information about the graph to make
+		a report if the user ask for it.
+	@param filename::ASCIIString Name or path of the file contain the graph
+		information. This function expect an ASCIIString.
 	"""	
 		# Instantiate the Class
 		this = new()
@@ -36,6 +44,10 @@ type simpleGraph
 
 		this.make_report = function(outfilename::ASCIIString)
 		"""
+		@brief: Method to make a small report with usefull informations
+			about the graph.
+		@param outfilename String with the name of the report filename
+			in the ASCIIString format. 
 		"""
 			# Open the file and write the information on it.
 			outfile = open(outfilename, "w")
@@ -50,7 +62,7 @@ type simpleGraph
 	end
 end
 
-#
+#@TODO : Implement a class to generate graph examples.
 type demoGraph
 	function demoGraph()
 		println("Inside the Class Demo Graph")
@@ -59,24 +71,30 @@ end
 
 function asAdjList(G::simpleGraph)
 """
-@description:
-@param G::simpleGraph :
-@output:
+@brief: Function to take the graph abstraction and transform into an 
+	Adjacency List.
+@param G::simpleGraph A simple graph containing the relations between
+	the edges (vertices index).
+@output: This function return the Adjacency List (Which is a List of List) for
+	the given simpleGraph.
 """
 end
 
 function asAdjMatrix(G::simpleGraph)
 """
-@description:
-@param G::simpleGraph :
-@output:
+@brief: Function to take the graph abstraction and transform into an 
+	Adjacency Matrix.
+@param G::simpleGraph A simple graph containing the relations between
+	the edges (vertices index). 
+@output: This function return the Adjacency Matrix (Which is a simple Matrix of
+	0's and 1's) for the given simpleGraph.
 """
-	vertex = simpleGraph.num_vertex
-	edges = simpleGraph.num_edge
+	vertex = G.num_vertex
+	edges = G.num_edge
 	adjmatrix = zeros(Int64, vertex, vertex)
 
 	@inbounds @simd for i in 1:edges
-    	rnum, cnum = simpleGraph.Graph[i,:]
+    	rnum, cnum = G.Graph[i,:]
     	adjmatrix[rnum,cnum] = 1
     end
 	return adjmatrix
